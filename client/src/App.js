95bed45b77795304597dfcd1e91a74c0e063403c
Mikepn02 <121401821+Mikepn02@users.noreply.github.com>
2023-07-22 14:07:48 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter , Navigate ,Routes , Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
+import Navbar from 'scenes/navbar';
 import ProfilePage from 'scenes/profilePage';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { CssBaseline ,ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material';
-import { themeSetting } from './theme.js';
+import { themeSettings } from './theme';
+import { createTheme } from '@mui/material/styles';
+import {CssBaseline,ThemeProvider} from '@mui/material';
+
+
+
+
 
 
 
@@ -15,16 +20,20 @@ function App() {
 
   const mode = useSelector((state) => state.mode);
   // that will help us to  grab the value we set in the state
-  const theme = useMemo(() => createTheme(themeSetting(mode)),[mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
+
+  console.log("mode",mode)
+
 
   return (
     <div className="App">
        <BrowserRouter>
        <ThemeProvider theme={theme} >
-        {/* <CssBaseline /> */}
+        <CssBaseline />
        <Routes>
           <Route path="/"  element={<LoginPage />}/>
           <Route path="/home"  element={<HomePage />}/>
+          <Route path="/nav"  element={<Navbar />}/>
           <Route path="/profile/:userId"  element={<ProfilePage />}/>
         </Routes>
        </ThemeProvider>
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-   mode: "Light",
+   mode: "light",
    user: null,
    token: null,
    posts : [],
